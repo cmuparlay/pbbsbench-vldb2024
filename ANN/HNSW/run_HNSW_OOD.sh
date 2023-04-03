863b7eb8b328906ac41751d1e4283b8ba408311b
Zheqi Shen <zheqi.shen@email.ucr.edu>
2023-04-03 02:27:03 -0700

    Revert "Adjust the script to make it run on the test platform"
    This reverts commit 3ec8268c6e0bf4381e648252d64fa3d5472baad8.

--- ANN/HNSW/run_HNSW_OOD.sh
+++ ANN/HNSW/run_HNSW_OOD.sh
@@ -1,4 +1,4 @@
-export RESULT_PREFIX="/ssd1/results/HNSW"
+export RESULT_PREFIX="temp/OOD"
 
 export dataset=
 export dtype=
@@ -17,17 +17,20 @@ export ef=10,15,20,30,50,75,100,250,500,1000,2000
 export beta=1
 export th=0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95,0.99,0.999
 
-export warmup=1
-export save_graph=1
+export warmup=0
+export save_graph=0
 export thread=`nproc`
-export limit_eval=1
+export limit_eval=0
 
 #-------------------------------------------------
 dataset=YandexT2I
 dtype=float
 dist=ndot
 
-dir_dataset=/ssd1/data/text2image1B
+dir_dataset=/data8/zshen055/Yandex-Text-to-Image
+#dir_dataset=/data2/zshen055/ANN/ANN_SIFT1B
+#file_in=$dir_dataset/base.1B.u8bin:u8bin
+#file_q=$dir_dataset/query.public.10K.u8bin:u8bin
 file_in=$dir_dataset/base.1B.fbin:fbin
 file_q=$dir_dataset/query.public.100K.fbin:fbin
 
@@ -37,7 +40,7 @@ alpha=1.1
 
 scale=1
 file_gt=$dir_dataset/text2image-1M:ubin
-bash run_HNSW_single.sh
+#bash run_HNSW_single.sh
 
 scale=10
 file_gt=$dir_dataset/text2image-10M:ubin
@@ -45,8 +48,8 @@ bash run_HNSW_single.sh
 
 scale=100
 file_gt=$dir_dataset/text2image-100M:ubin
-bash run_HNSW_single.sh
+#bash run_HNSW_single.sh
 
 scale=1000
 file_gt=$dir_dataset/text2image-1B:ubin
-bash run_HNSW_single.sh
+#bash run_HNSW_single.sh
