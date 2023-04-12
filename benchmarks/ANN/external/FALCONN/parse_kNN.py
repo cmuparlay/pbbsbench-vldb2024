1fcb565c465d3ef2a701109c41ee6ca009a0d8fc
Zheqi Shen <zheqi.shen@email.ucr.edu>
2023-04-12 04:56:49 -0700

    Support to parse FALCONN logs into csv files
    run_FALCONN_single.sh: auto append the parsed results to the csv file

--- benchmarks/ANN/external/FALCONN/parse_kNN.py
+++ benchmarks/ANN/external/FALCONN/parse_kNN.py
@@ -1,10 +1,12 @@
 import sys
 import re
 import functools
+import csv
 
-p_cmd = re.compile(r"\./calc_recall -n ([0-9]*)")
+p_cmd = re.compile(r"\./calc_recall -n ([0-9]*) .*?-lsh (.*?) .*?-l ([0-9]*) .*?-rot ([0-9]*)")
 p_dim = re.compile(r"size: ([0-9]*)")
 p_algo = re.compile(r"Start building (.*)")
+p_num_hashbit = re.compile(r"num_hashbit: ([0-9]*)")
 p_buildtime = re.compile(r"Build index: (.*)")
 p_read_gt = re.compile(r"Read groundTruthFile")
 p_qparam = re.compile(r"measure recall@([0-9]*) with num_probes=([0-9]*) on ([0-9]*) queries")
@@ -14,7 +16,8 @@ p_avg_uniq_cand = re.compile(r"average number of unique candidates: (.*)")
 p_endq = re.compile(r"---")
 
 file_in = sys.argv[1]
-target_k = int(sys.argv[2]) if len(sys.argv)>2 else None
+target_k = int(sys.argv[2]) if len(sys.argv)>2 else 0
+file_out = sys.argv[3] if len(sys.argv)>3 else None
 
 preamble = {}
 building = {}
@@ -30,6 +33,9 @@ for l in f.readlines():
 			n = int(res.group(1))
 			preamble["scale"] = n
 			preamble["scale_M"] = n/1000000
+			building["lsh"] = res.group(2)
+			building["l"] = int(res.group(3))
+			building["rot"] = int(res.group(4))
 			continue
 		res = re.search(p_dim, l)
 		if res is not None: # algorithm name
@@ -38,6 +44,10 @@ for l in f.readlines():
 		res = re.search(p_algo, l)
 		if res is not None: # algorithm name
 			preamble["algo"] = res.group(1)
+			continue
+		res = re.search(p_num_hashbit, l)
+		if res is not None: # #hashbit
+			preamble["num_hashbit"] = int(res.group(1))
 			continue
 		res = re.search(p_buildtime, l)
 		if res is not None: # building time
@@ -74,7 +84,7 @@ for l in f.readlines():
 			continue
 		res = re.match(p_endq, l)
 		if res is not None: # at the end of current query
-			if target_k is None or q["k"]==target_k:
+			if target_k==0 or q["k"]==target_k:
 				query.append(q)
 			q = {}
 
@@ -89,10 +99,9 @@ query.sort(key=lambda q: q["recall"])
 
 print("max_cand\trecall\t\tQPS\t\tucand\tnum_probes")
 # bucket = [0.02,0.05,0.06,0.08,0.1,0.12,0.14,0.16,0.18,0.2, 0.22,0.23,0.25,0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 0.99, 0.999, 1]
-bucket = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 0.99, 0.999, 1]
-recall_in_bucket = []
-QPS_in_bucket = []
-ucand_in_bucket = []
+# bucket = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 0.99, 0.999, 1]
+bucket = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.65, 0.7, 0.75, 0.8, 0.83, 0.85, 0.87, 0.9, 0.95, 0.97, 0.99, 0.995, 0.999, 1]
+q_in_bucket = []
 for j in range(len(bucket)-1):
 	b = bucket[j]
 	l = 0
@@ -137,15 +146,49 @@ for j in range(len(bucket)-1):
 	if len(candidate)>0:
 		q = functools.reduce(lambda x,y: x if x["QPS"]>y["QPS"] else y, candidate)
 		print("%d\t\t%f\t%f\t%d\t%d"%(q["max_cand"],q["recall"],q["QPS"],int(q["avg_uniq_cand"]),int(q["num_probes"])))
-		recall_in_bucket.append(q["recall"])
-		QPS_in_bucket.append(q["QPS"])
-		ucand_in_bucket.append(q["avg_uniq_cand"])
+		q_in_bucket.append((q,b))
+		# recall_in_bucket.append(q["recall"])
+		# QPS_in_bucket.append(q["QPS"])
+		# ucand_in_bucket.append(q["avg_uniq_cand"])
 	"""
 	for q in candidate:
 		print("%d %f %f"%(q["ef"],q["recall"],q["QPS"]))
 		pass
 	"""
 
-print(",".join([str(k) for k in recall_in_bucket]))
-print(",".join([str(k) for k in QPS_in_bucket]))
-print(",".join([str(int(k)) for k in ucand_in_bucket]))
+print(",".join([str(q["recall"]) for q,b in q_in_bucket]))
+print(",".join(["%.2f"%q["QPS"] for q,b in q_in_bucket]))
+print(",".join([str(int(q["avg_uniq_cand"])) for q,b in q_in_bucket]))
+
+if file_out is not None:
+	with open(file_out, "a", newline='') as csvfile:
+		writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+		writer.writerow(["Algorithm","Parameters","Size","Build time","Hash bits"])
+		writer.writerow([
+			preamble["algo"],
+			"lsh = %s, l = %d, rot = %d" % (building["lsh"],building["l"],building["rot"]),
+			preamble["scale"],
+			round(building["time"], 3),
+			preamble["num_hashbit"]
+		]);
+		writer.writerow([])
+		writer.writerow([
+			"Num queries","Target recall","Actual recall","QPS",
+			"Average Cands","Average Uniq Cands","Num probes","Cand limit"
+		])
+
+		for i in range(len(q_in_bucket)):
+			q,b = q_in_bucket[i]
+			if i+1<len(q_in_bucket) and q==q_in_bucket[i+1][0]:
+				continue
+			writer.writerow([
+				q["cnt"],
+				b,
+				q["recall"],
+				round(q["QPS"], 2),
+				int(q["avg_cand"]),
+				int(q["avg_uniq_cand"]),
+				q["num_probes"],
+				q["max_cand"],
+			])
+		writer.writerow([])
--- benchmarks/ANN/external/FALCONN/run_FALCONN_single.sh
+++ benchmarks/ANN/external/FALCONN/run_FALCONN_single.sh
@@ -16,12 +16,13 @@
 # lastk
 # lc
 # (rad)
-RESULT_PATH=${RESULT_PREFIX}/FALCONN/$dataset/L${L}_rot${rot}_K${K}_${lsh}${lastk}_${dist}
+algo="FALCONN"
+RESULT_DIR=${RESULT_PREFIX}/${algo}/$dataset/L${L}_rot${rot}_K${K}_${lsh}${lastk}_${dist}
 
 #set -x
 date
 
-mkdir -p $RESULT_PATH
+mkdir -p $RESULT_DIR
 
 echo "Running for the first ${scale} million points on ${dataset}"
 param_basic="-n $((scale*1000000)) -type ${dtype}"
@@ -31,6 +32,26 @@ param_other="-rot ${rot}"
 if [ -n "$rad" ]; then
 	param_other="${param_other} -rad ${rad}"
 fi
-echo "./calc_recall ${param_basic} ${param_building} ${param_query} ${param_other} > ${RESULT_PATH}/${scale}M.log 2>&1"
-`which time` -v ./calc_recall ${param_basic} ${param_building} ${param_query} ${param_other} > ${RESULT_PATH}/${scale}M.log 2>&1
 
+LOG_PATH=${RESULT_DIR}/${scale}M.log
+echo "./calc_recall ${param_basic} ${param_building} ${param_query} ${param_other} > ${LOG_PATH} 2>&1"
+`which time` -v ./calc_recall ${param_basic} ${param_building} ${param_query} ${param_other} > ${LOG_PATH} 2>&1
+
+shortname=$(echo $dataset | tr '[:upper:]' '[:lower:]')
+if [[ "$dataset" == "BIGANN" ]]; then
+	shortname="bigann"
+elif [[ "$dataset" == "MSSPACEV" ]]; then
+	shortname="spacev"
+elif [[ "$dataset" == "YandexT2I" ]]; then
+	shortname="t2i"
+elif [[ "$dataset" == "FB_ssnpp" ]]; then
+	shortname="ssnpp"
+fi
+CSV_PATH=${RESULT_PREFIX}/${shortname}_${algo}.csv
+if [ -n "$rad" ]; then
+	echo "python3 parse_range.py ${LOG_PATH} ${CSV_PATH}"
+	python3 parse_range.py ${LOG_PATH} ${CSV_PATH}
+else
+	echo "python3 parse_kNN.py ${LOG_PATH} 0 ${CSV_PATH}"
+	python3 parse_kNN.py ${LOG_PATH} 0 ${CSV_PATH}
+fi
